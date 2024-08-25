985d380f058361db4f6322b9adb67ed3114f2c19
Paul Leopardi <paul.leopardi@anu.edu.au>
2024-08-25 10:50:23 +1000

    Repair description. Tidy up.

--- eq_histogram/eq_count_points_by_s2_region.m
+++ eq_histogram/eq_count_points_by_s2_region.m
@@ -13,27 +13,27 @@ function count_v = eq_count_points_by_s2_region(s_point,N)
 %    s_point contained in each region.
 %
 %Arguments
-% N        Required number of regions, a positive integer.
-% s_points Sequence of points on S^2, as a 2 x n_points array in spherical polar coordinates,
+% s_point  Sequence of points on S^2, as a 2 x n_points array in spherical polar coordinates,
 %          with longitude 0 <= s(1,p_idx) <= 2*pi, colatitude 0 <= s(2,p_idx) <= pi.
+% N        Required number of regions, a positive integer.
 %
 %Examples
 %  > points_x=randn(3,8)
 %  points_x =
-%  
+%
 %    -1.500867  -0.994941  -0.466281  -2.419762  -0.027346  -0.362112   0.070776  -1.106204
 %    -0.655976  -1.022458   0.669978  -1.066180  -0.025138   2.686040   1.195864  -0.087131
 %    -0.273793  -0.965146  -1.040648   0.311548  -0.788428   1.790929   0.532415   0.919639
-%  
+%
 %  > points_s=cart2polar2(points_x)
 %  points_s =
-%  
+%
 %     3.55364   3.94063   2.17881   3.55661   3.88495   1.70480   1.51168   3.22020
 %     1.73642   2.16558   2.47645   1.45352   3.09452   0.98688   1.15258   0.87875
-%  
+%
 %  > count_v=eq_count_points_by_s2_region(points_s,5)
 %  count_v =
-%  
+%
 %     1   2   3   0   2
 %
 %See also
