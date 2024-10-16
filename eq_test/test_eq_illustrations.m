cd9f6e0523c968bf15048f8801141670f3d8823b
Paul Leopardi <paul.leopardi@anu.edu.au>
2024-10-16 23:52:16 +1100

    Close figures between tests

--- eq_test/test_eq_illustrations.m
+++ eq_test/test_eq_illustrations.m
@@ -2,17 +2,25 @@ function test_eq_illustrations
 %TEST_EQ_ILLUSTRATIONS Test the functions in the eq_illustrations directory
 
 % Copyright 2024 Paul Leopardi.
-% $Revision $ 1.12 $ $Date 2024-10-14 $
+% $Revision $ 1.12 $ $Date 2024-10-16 $
 %
 % For licensing, see COPYING.
 % For references, see AUTHORS.
 % For revision history, see CHANGELOG.
 
 test_haslight
+close all
 test_illustrate_eq_algorithm
+close all
 test_illustration_options
+close all
 test_project_point_set
+close all
 test_project_s2_partition
+close all
 test_project_s3_partition
+close all
 test_show_r3_point_set
+close all
 test_show_s2_partition
+close all
