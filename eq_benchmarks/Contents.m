1b7470de321432014df1705c43d9d2cf04b3a1d7
Paul Leopardi <paul.leopardi@anu.edu.au>
2026-04-14 22:03:17 +1000

    Commit previously untracked files

---
+++ eq_benchmarks/Contents.m
@@ -0,0 +1,25 @@
+%Recursive Zonal Equal Area Sphere Partitioning: Benchmarks
+%
+% Recursive Zonal Equal Area (EQ) Sphere Partitioning Toolbox.
+% Release 1.12.3 2026-04-14
+%
+%Functions
+%=========
+%
+% run_eq_benchmarks       Run all performance benchmarks and log results
+%
+% Individual benchmarks (in src/ directory)
+% -----------------------------------------
+% benchmark_area_error    Benchmark for eq_area_error
+% benchmark_energy_dist   Benchmark for eq_energy_dist
+% benchmark_eq_regions    Performance benchmark for eq_regions
+% benchmark_histograms    Benchmark for point-to-region mapping (histogram)
+% benchmark_mindist       Benchmark for eq_min_dist
+% benchmark_sradius       Benchmark for sradius_of_cap
+
+% Copyright 2026 Paul Leopardi.
+% $Revision 1.12.3 $ $Date 2026-04-14 $
+%
+% For licensing, see COPYING.
+% For references, see AUTHORS.
+% For revision history, see CHANGELOG.
